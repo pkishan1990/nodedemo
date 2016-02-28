1cc04f0fc6c898c2b767a3ea6fd46393c8a159b7
Kishan Patel <pkishan1990@gmail.com>
2016-02-28 12:32:51 +0530

    test

--- pba.js
+++ pba.js
@@ -5,6 +5,26 @@ basic bank account
 1. open account
 2. deposit money -> function deposit
 3. withdraw money -> function withdraw
-4. how much money in account
+4. how much money in account //getBalance
  */
+var account={
+    balance:0
+};
+console.log('Initial Deposit: ' +account.balance);
 
+function deposit(account,amount){
+    account.balance+=amount;
+}
+
+function withdraw(account,amount){
+    account.balance-=amount;
+}
+
+function getBalance(account){
+    return account.balance;
+}
+
+deposit(account, 5000);
+withdraw(account, 2000);
+
+console.log(getBalance(account));
