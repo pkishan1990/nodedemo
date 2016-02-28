ae3b3ee80c3dcffd92d33298e695b8352bd61da3
Kishan Patel <pkishan1990@gmail.com>
2016-02-28 11:57:23 +0530

    test

--- pba.js
+++ pba.js
@@ -3,8 +3,8 @@ creating function, object, variable.
 basic bank account
 
 1. open account
-2. deposit money
-3. widraw money
+2. deposit money -> function deposit
+3. withdraw money -> function withdraw
 4. how much money in account
  */
 
